f22556f0cd6307954a941b2dd683fea722ff8c47
dimas <dimasrizkyaf@gmail.com>
2024-02-12 15:09:31 +0700

    Setup env

---
+++ Assets/Scenes/SetupEnv.cs
@@ -0,0 +1,31 @@
+using CandyCoded.env;
+using UnityEngine;
+
+public class SetupEnv : MonoBehaviour
+{
+    private readonly string filename = "prod";
+
+    private void Awake()
+    {
+        env.SetFilename(filename);
+    }
+
+    void Start()
+    {
+
+        if (env.TryParseEnvironmentVariable("API_HOST", out string host))
+        {
+            Debug.Log($"Host is: {host}");
+        }
+
+        if (env.TryParseEnvironmentVariable("API_PORT", out string port))
+        {
+            Debug.Log($"Port is: {port}");
+        }
+
+        if (env.TryParseEnvironmentVariable("DEBUG", out bool isDebug))
+        {
+            Debug.Log($"Debug Mode is: {(isDebug ? "ON" : "OFF")}");
+        }
+    }
+}
